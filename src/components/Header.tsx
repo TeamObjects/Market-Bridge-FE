84ea5883404c7dbdf93df28abef4e6728dbc91c3
pangkyu <baesense@naver.com>
2024-01-24 23:11:59 +0900

    Design : Header UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,66 +10,55 @@ import {
 
 const Header: React.FC = () => {
   return (
-    <header className="flex w-[100%]">
-      <div className="flex w-[15%] h-[10rem] justify-center items-center">
+    <header className="flex flex-row w-[100%] h-[10rem] text-[1.6rem]">
+      <div className="flex w-[25%] justify-center items-center">
         <Image
           src={BridgeLogo}
           alt="브릿지로고"
           className="w-[10rem] h-[7rem]"
         />
       </div>
-      <div className="flex w-[40%]">
-        <div className="flex justify-center items-center cursor-pointer">
+      <div className="flex w-[30%]">
+        <div className="flex justify-center items-center cursor-pointer flex-grow">
           <Image src={Category} alt="카테고리" />
-          <Link href="/">카테코리</Link>
+          <p>카테코리</p>
         </div>
-        <Link
-          href="/"
-          className="flex justify-center items-center cursor-pointer"
-        >
+        <Link href="/" className="flex justify-center items-center flex-grow">
           컬리추천
         </Link>
-        <Link
-          href="/"
-          className="flex justify-center items-center cursor-pointer"
-        >
+        <Link href="/" className="flex justify-center items-center flex-grow">
           신상품
         </Link>
-        <Link
-          href="/"
-          className="flex justify-center items-center cursor-pointer"
-        >
+        <Link href="/" className="flex justify-center items-center flex-grow">
           베스트
         </Link>
-        <Link
-          href="/"
-          className="flex justify-center items-center cursor-pointer"
-        >
+        <Link href="/" className="flex justify-center items-center flex-grow">
           알뜰쇼핑
         </Link>
-        <Link
-          href="/"
-          className="flex justify-center items-center cursor-pointer"
-        >
+        <Link href="/" className="flex justify-center items-center flex-grow">
           특가/혜택
         </Link>
       </div>
-      <div className="flex w-[20%]">
+      <div className="flex w-[20%] justify-center items-center">
         <input
           type="text"
-          placeholder="검색어를 입력하세요"
-          className="flex border bg-[url('/public/svgs/magnifier.svg')]"
+          placeholder="검색어를 입력하세요."
+          className="flex w-[90%] h-[40%] border border-[#03BAF2] rounded-full"
         />
       </div>
-      <div className="flex w-[20%]">
+      <div className="flex w-[20%] justify-around items-center ">
         <Link href="/">회원가입</Link>
         <Link href="/">로그인</Link>
         <Link href="/">고객센터</Link>
-        <Image src={Mypage} alt="마이페이지" className="w-[10rem] h-[7rem]" />
+        <Image
+          src={Mypage}
+          alt="마이페이지"
+          className="flex w-[3rem] h-[5rem]"
+        />
         <Image
           src={ShoppingBasket}
           alt="장바구니"
-          className="w-[10rem] h-[7rem]"
+          className="flex w-[3rem] h-[5rem]"
         />
       </div>
     </header>
