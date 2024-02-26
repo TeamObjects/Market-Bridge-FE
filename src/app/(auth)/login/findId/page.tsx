28a60121d2d913333900d30b026c7fa1a16562fa
DataCodeLiteracy <dataliteracy@icloud.com>
2024-02-27 00:40:45 +0900

    [ ♻️  | #17 | refactor ] 아이디, 비밀번호 찾기 UI 변경
    2024.02.27

--- src/app/(auth)/login/findId/page.tsx
+++ src/app/(auth)/login/findId/page.tsx
@@ -1,15 +1,21 @@
 import FindAuthForm from '@/components/(auth)/(findAuth)/FindAuthForm';
-import FindAuthNav from '@/components/(auth)/(findAuth)/FindAuthNav';
 import FindAuthPage from '@/components/(auth)/(findAuth)/FindAuthPage';
 import FoundAuthPage from '@/components/(auth)/(findAuth)/FoundAuthPage';
 import FoundIdHeader from '@/components/(auth)/(findAuth)/FoundIdHeader';
+import AuthHeader from '@/components/(auth)/AuthHeader';
 import FormContext from '@/contexts/FormContext';
 
 const FindId = () => {
   return (
     <>
-      <FindAuthPage header={<h1 className="text-3xl my-14">아이디 찾기</h1>}>
-        <FindAuthNav />
+      <FindAuthPage
+        header={
+          <>
+            <AuthHeader />
+            <h1 className="text-center text-3xl my-14">아이디 찾기</h1>
+          </>
+        }
+      >
         <FormContext id="findId-form" formType="findId">
           <FindAuthForm />
         </FormContext>
