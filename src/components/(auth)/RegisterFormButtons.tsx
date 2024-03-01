786da0383cfe985a90c557f82eba2291cfd43dc7
DataCodeLiteracy <dataliteracy@icloud.com>
2024-03-02 03:11:53 +0900

    [ ♻️  | #20 | refactor ] 중복된 이메일이거나 유효성 검사 통과하지 않았을 때 계속 버튼 disabled 처리
    2024.03.02

--- src/components/(auth)/RegisterFormButtons.tsx
+++ src/components/(auth)/RegisterFormButtons.tsx
@@ -3,17 +3,36 @@
 import Button from '@/components/shared/Button';
 
 import { FormValue, formContext } from '@/contexts/FormContext';
+import authState from '@/recoil/authAtom';
 
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
 
 const RegisterFormButtons = () => {
-  const { isEnterUserInfo, handleClickContinue } = useContext(
+  const { isEnterUserInfo, isValid, handleClickContinue } = useContext(
     formContext,
   ) as FormValue;
+  const [isAllValidCheck, setIsAllValidCheck] = useState(false);
+
+  const [isDuplicateEmail, _] = useRecoilState(authState);
+
+  useEffect(() => {
+    if (isValid) {
+      setIsAllValidCheck(
+        Object.values(isValid).every((value) => value === true),
+      );
+    }
+  }, [isValid]);
 
   return (
     <>
-      {!isEnterUserInfo && <Button text="계속" onClick={handleClickContinue} />}
+      {!isEnterUserInfo && (
+        <Button
+          text="계속"
+          onClick={handleClickContinue}
+          disabled={!!isDuplicateEmail || !isAllValidCheck}
+        />
+      )}
       {isEnterUserInfo && <Button text="가입하기" type="submit" />}
     </>
   );
