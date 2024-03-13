78afaf79d918b56a6f5cf3e34dad3caa76f1a011
pangkyu <baesense@naver.com>
2024-03-13 22:56:46 +0900

    Fix : 이미지 도메인 설정 수정

--- next.config.js
+++ next.config.js
@@ -2,13 +2,12 @@
 
 const nextConfig = {
   images: {
-    domains: (() => {
-      const domains = [];
-      for (let i = 0; i < 49; i++) {
-        domains.push(`thumbnail${i}.coupangcdn.com`);
-      }
-      return domains;
-    })(),
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.coupangcdn.com',
+      },
+    ],
   },
 };
 
