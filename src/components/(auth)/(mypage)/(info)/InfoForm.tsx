079a94edebe9ee16dcbb912d67d6f4176a777b99
DataCodeLiteracy <dataliteracy@icloud.com>
2024-03-14 17:38:19 +0900

    [ ✨ | #24 | feature ] 회원정보확인 기능 구현
    2024.03.14

--- src/components/(auth)/(mypage)/(info)/InfoForm.tsx
+++ src/components/(auth)/(mypage)/(info)/InfoForm.tsx
@@ -1,13 +1,83 @@
+'use client';
+
+import { findEmailInfo, getAccountInfo } from '@/api/mypageApi';
+
 import InfoTextField from '@/components/(auth)/(mypage)/(info)/InfoTextField';
 
+import { useQuery } from '@tanstack/react-query';
+
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+import { useRouter } from 'next/navigation';
+import InfoButton from './InfoButton';
+import useAlertContext from '@/hooks/useAlertContext';
+
 const InfoForm = () => {
+  const [password, setPassword] = useState('');
+
+  const router = useRouter();
+
+  const { data } = useQuery({
+    queryKey: ['account'],
+    queryFn: findEmailInfo,
+  });
+
+  const email = data?.data.email;
+
+  const { data: accountData } = useQuery({
+    queryKey: ['account'],
+    queryFn: () => getAccountInfo(password),
+  });
+
+  const { open, close } = useAlertContext();
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (password.trim().length > 0) {
+      const response = await getAccountInfo(password);
+
+      if (response.code === 200) {
+        router.push('/mypage/info/modify');
+      }
+    } else {
+      open({
+        title: '비밀번호를 입력해주세요.',
+        onRightButtonClick: () => {
+          close();
+        },
+      });
+    }
+  };
+
   return (
-    <form className="flex flex-col justify-center items-center">
-      <InfoTextField name="아이디" />
+    <form
+      className="flex flex-col justify-center items-center"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex justify-center items-center w-full px-[20px] py-[10px]">
+        <label
+          htmlFor="name"
+          id="name"
+          className="w-[120px] text-[16px] font-semibold"
+        >
+          아이디
+        </label>
+        <span className="flex items-center w-[305px] h-[50px] pl-8 text-[16px]">
+          {email}
+        </span>
+      </div>
       <InfoTextField
+        type="password"
         name="비밀번호"
         placeholder="현재 비밀번호를 입력해주세요"
+        onChange={handlePasswordChange}
       />
+      <InfoButton type="submit" />
     </form>
   );
 };
--- src/components/(auth)/(mypage)/(info)/InfoModifyForm.tsx
+++ src/components/(auth)/(mypage)/(info)/InfoModifyForm.tsx
@@ -3,21 +3,32 @@ import InfoTextField from './InfoTextField';
 const InfoModifyForm = () => {
   return (
     <form>
-      <InfoTextField name="아이디" />
+      <InfoTextField type="text" name="아이디" />
       <InfoTextField
+        type="password"
         name="현재 비밀번호"
         placeholder="비밀번호를 입력해주세요"
       />
       <InfoTextField
+        type="password"
         name="새 비밀번호"
         placeholder="새 비밀번호를 입력해주세요"
       />
       <InfoTextField
+        type="password"
         name="새 비밀번호 확인"
         placeholder="새 비밀번호를 다시 입력해 주세요"
       />
-      <InfoTextField name="이름" placeholder="이름을 입력해주세요" />
-      <InfoTextField name="휴대폰" placeholder="휴대폰 번호를 입력해주세요" />
+      <InfoTextField
+        type="text"
+        name="이름"
+        placeholder="이름을 입력해주세요"
+      />
+      <InfoTextField
+        type="text"
+        name="휴대폰"
+        placeholder="휴대폰 번호를 입력해주세요"
+      />
     </form>
   );
 };
