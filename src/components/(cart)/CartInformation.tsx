b61909dacb7fd7879ba12b7a14a2a6e0514ed5cd
pangkyu <baesense@naver.com>
2024-03-19 10:28:52 +0900

    Feat : 배송지 주소 기능

--- src/components/(cart)/CartInformation.tsx
+++ src/components/(cart)/CartInformation.tsx
@@ -1,7 +1,24 @@
+'use client';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 import { Location } from '../../../public/svgs';
+import { useQuery } from '@tanstack/react-query';
+import { getAddressList } from '@/api/mypageApi';
+import { AddressItemResponse } from '../(auth)/(mypage)/(address)/AddressListItems';
 
 const CartInformation = () => {
+  const { data } = useQuery<AddressItemResponse, Error>({
+    queryKey: ['address'],
+    queryFn: getAddressList,
+  });
+  const [address, setAddress] = useState<string>();
+
+  if (data && !address) {
+    const { city, street, detail, zipcode } = data?.data[0].addressValue;
+    setAddress(`${city} ${street} ${detail} ${zipcode}`);
+  }
+
   return (
     <aside className="flex flex-col w-[30%] h-[80%] items-center ">
       <div className="flex flex-col w-[80%] h-[40%] border justify-around items-center">
@@ -10,11 +27,27 @@ const CartInformation = () => {
           <p>배송지</p>
         </div>
         <div className="flex w-[90%] h-[20%] items-center text-2xl">
-          <p>경기도 성남시 분당구 정자동 불정로 6 NAVER 그린팩토리 1층</p>
+          {data?.data.length === 0 ? (
+            <p>등록된 주소가 없습니다.</p>
+          ) : (
+            <p>{address}</p>
+          )}
         </div>
-        <div className="flex w-[90%] h-[20%] justify-center items-center text-xl border cursor-pointer">
-          배송지 변경
-        </div>
+        {data?.data.length === 0 ? (
+          <Link
+            href={'/mypage/address'}
+            className="flex w-[90%] h-[20%] justify-center items-center text-xl border cursor-pointer"
+          >
+            배송지 등록
+          </Link>
+        ) : (
+          <Link
+            href={'/mypage/address'}
+            className="flex w-[90%] h-[20%] justify-center items-center text-xl border cursor-pointer"
+          >
+            배송지 변경
+          </Link>
+        )}
       </div>
       <div className="flex flex-col w-[80%] h-[50%] border bg-[#FAFAFA] items-center text-2xl">
         <div className="flex flex-col w-[90%] h-[60%] justify-center">
